8823d04fdc914e06b6b560a0a420f5231232ac9b
Ridi Otoko <rdplus2015@gmail.com>
2026-07-31 23:17:32 -0400

    feat(AdminLayout.tsx) component added

---
+++ ui-app/src/components/backOffice/AdminLayout.tsx
@@ -0,0 +1,27 @@
+// AdminLayout.tsx
+import { ReactNode } from "react"
+import { AdminSidebar } from "./AdminSidebar"
+
+interface AdminLayoutProps {
+  children: ReactNode
+}
+
+export function AdminLayout({ children }: AdminLayoutProps) {
+  return (
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-4" type="checkbox" className="drawer-toggle inline" />
+      <div className="drawer-content">
+        <nav className="navbar w-full bg-base-300">
+          <label htmlFor="my-drawer-4" aria-label="open sidebar" className="btn btn-square btn-ghost drawer-button">
+            {/* icon toggle */}
+          </label>
+          <div className="px-4">Admin</div>
+        </nav>
+
+        <div className="p-4">{children}</div>
+      </div>
+
+      <AdminSidebar />
+    </div>
+  )
+}
