09a68dd069da302e01ed90ffaa1f6ad52f93d8dd
Ridi Otoko <rdplus2015@gmail.com>
2026-08-06 18:21:18 -0400

    feat(projects- serializer.py) ProjectSerializer added

--- backend-app/portfolio/career/serializer.py
+++ backend-app/portfolio/career/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
-from career.models import Education
+from career.models import Education, Experience
+from projects.Serializer import SkillSerializer
+from projects.models import Skill
 
 
 class EducationSerializer(serializers.ModelSerializer):
@@ -12,4 +14,25 @@ class EducationSerializer(serializers.ModelSerializer):
             "start_date", "end_date",
             "issued_date", "in_progress",
             "badge_image", "credential_url", "order"
+        ]
+
+
+class ExperienceSerializer(serializers.ModelSerializer):
+    technologies = SkillSerializer(many=True, read_only=True)
+    technology_ids = serializers.PrimaryKeyRelatedField(
+        queryset=Skill.objects.all(),
+        many=True,
+        write_only=True,
+        source="technologies"
+    )
+
+    class Meta:
+        model = Experience
+        fields = [
+            "id", "title", "company",
+            "location", "type",
+            "start_date", "end_date",
+            "description", "company_logo",
+            "technologies", "technology_ids",
+            "order"
         ]
--- backend-app/portfolio/projects/Serializer.py
+++ backend-app/portfolio/projects/Serializer.py
@@ -1,9 +1,36 @@
+
 from rest_framework import serializers
 
-from projects.models import Skill
+from projects.models import Project, Skill
+
 
 
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
         fields = ["id", "name", "category", "order"]
+
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    skills = SkillSerializer(many=True, read_only=True)
+    skill_ids = serializers.PrimaryKeyRelatedField(
+        queryset=Skill.objects.all(),
+        many=True,
+        write_only=True,
+        source="skills"
+    )
+
+    class Meta:
+        model = Project
+        fields = [
+            "id", "title", "slug",
+            "description", "long_description",
+            "backend_image", "frontend_image", "deployment_image",
+            "github_url", "live_url", "linkedin_url",
+            "tags",
+            "skills", "skill_ids",
+            "featured", "order",
+            "created_at", "updated_at",
+        ]
+        read_only_fields = ["created_at", "updated_at"]
